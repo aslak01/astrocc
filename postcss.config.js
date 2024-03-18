512e2101fbd54f4d70d2b61bf11daee19cad2af9
as <aslak.bakkeland@medialaben.no>
2024-03-18 23:29:13 +0100

    open-props config

---
+++ postcss.config.js
@@ -0,0 +1,27 @@
+import openProps from "open-props";
+import postcssBundler from "@csstools/postcss-bundler";
+import postcssMinify from "@csstools/postcss-minify";
+
+import postcssJitProps from "postcss-jit-props";
+
+import postcssGlobalData from "@csstools/postcss-global-data";
+import postcssCustomMedia from "postcss-custom-media";
+
+import postcssPresetEnv from "postcss-preset-env";
+
+const config = {
+  plugins: [
+    postcssBundler(),
+    postcssGlobalData({
+      files: ["node_modules/open-props/media.min.css"],
+    }),
+    postcssCustomMedia({
+      preserve: false,
+    }),
+    postcssJitProps(openProps),
+    postcssPresetEnv(),
+    postcssMinify(),
+  ],
+};
+
+export default config;
